8fd38d351de3c736e4c35c0dd7909833ae211071
Victor de Almeida <shoosegames@gmail.com>
2021-05-28 18:59:01 -0300

    Add Charts

---
+++ src/Components/Reports/barChart.js
@@ -0,0 +1,116 @@
+import React from "react";
+
+import { ResponsiveBar } from "@nivo/bar";
+
+function BarChart() {
+  const data = [
+    {
+      Estrelas: "0 Estrela",
+      Votos: 2,
+    },
+    {
+      Estrelas: "1 Estrelas",
+      Votos: 14,
+    },
+    {
+      Estrelas: "2 Estrelas",
+      Votos: 15,
+    },
+    {
+      Estrelas: "3 Estrelas",
+      Votos: 18,
+    },
+    {
+      Estrelas: "4 Estrelas",
+      Votos: 24,
+    },
+    {
+      Estrelas: "5 Estrelas",
+      Votos: 62,
+    },
+  ];
+
+  return (
+    <ResponsiveBar
+      data={data}
+      keys={["Votos"]}
+      colors="#363740"
+      borderColor="#fff"
+      indexBy="Estrelas"
+      width={1000}
+      height={350}
+      margin={{ top: 25, right: 80, bottom: 60, left: 50 }}
+      padding={0.3}
+      valueScale={{ type: "linear" }}
+      indexScale={{ type: "band", round: true }}
+      defs={[
+        {
+          id: "dots",
+          type: "patternDots",
+          background: "inherit",
+          size: 4,
+          padding: 1,
+          stagger: true,
+        },
+        {
+          id: "lines",
+          type: "patternLines",
+          background: "inherit",
+          rotation: -45,
+          lineWidth: 6,
+          spacing: 10,
+        },
+      ]}
+      axisTop={null}
+      axisRight={null}
+      axisBottom={{
+        tickSize: 5,
+        tickPadding: 5,
+        tickRotation: 0,
+        legend: "Estrelas",
+        legendPosition: "middle",
+        legendOffset: 32,
+      }}
+      axisLeft={{
+        tickSize: 5,
+        tickPadding: 5,
+        tickRotation: 0,
+        legend: "Votos",
+        legendPosition: "middle",
+        legendOffset: -40,
+      }}
+      labelSkipWidth={12}
+      labelSkipHeight={12}
+      labelTextColor="#f9f9f9"
+      legends={[
+        {
+          dataFrom: "keys",
+          anchor: "bottom-right",
+          direction: "column",
+          justify: false,
+          translateX: 120,
+          translateY: 0,
+          itemsSpacing: 2,
+          itemWidth: 100,
+          itemHeight: 20,
+          itemDirection: "left-to-right",
+          itemOpacity: 0.85,
+          symbolSize: 20,
+          effects: [
+            {
+              on: "hover",
+              style: {
+                itemOpacity: 1,
+              },
+            },
+          ],
+        },
+      ]}
+      animate={true}
+      motionStiffness={90}
+      motionDamping={15}
+    />
+  );
+}
+
+export default BarChart;
--- src/Pages/Reports.js
+++ src/Pages/Reports.js
@@ -1,12 +1,14 @@
 import React, { useContext, useEffect, useState } from "react";
 import { Button } from "primereact/button";
 import { Calendar } from "primereact/calendar";
+import { addLocale } from "primereact/api";
 import moment from "moment";
 import "moment/locale/pt-br";
 
 import { AuthContext } from "../Configs/ContextProvider";
 
 import "../Styles/Report.css";
+import BarChart from "../Components/Reports/barChart";
 
 function Reports() {
   const { setCurrentPage } = useContext(AuthContext);
@@ -28,14 +30,77 @@ function Reports() {
     setSearchEndDate(endDate);
   }
 
+  addLocale("pt", {
+    closeText: "Fechar",
+    prevText: "Anterior",
+    nextText: "Próximo",
+    currentText: "Começo",
+    monthNames: [
+      "Janeiro",
+      "Fevereiro",
+      "Março",
+      "Abril",
+      "Maio",
+      "Junho",
+      "Julho",
+      "Agosto",
+      "Setembro",
+      "Outubro",
+      "Novembro",
+      "Dezembro",
+    ],
+    monthNamesShort: [
+      "Jan",
+      "Fev",
+      "Mar",
+      "Abr",
+      "Mai",
+      "Jun",
+      "Jul",
+      "Ago",
+      "Set",
+      "Out",
+      "Nov",
+      "Dez",
+    ],
+    dayNames: [
+      "Domingo",
+      "Segunda",
+      "Terça",
+      "Quarta",
+      "Quinta",
+      "Sexta",
+      "Sábado",
+    ],
+    dayNamesShort: ["Dom", "Seg", "Ter", "Qua", "Qui", "Sex", "Sáb"],
+    dayNamesMin: ["D", "S", "T", "Q", "Q", "S", "S"],
+    weekHeader: "Semana",
+    firstDay: 1,
+    isRTL: false,
+    showMonthAfterYear: false,
+    yearSuffix: "",
+    timeOnlyTitle: "Só Horas",
+    timeText: "Tempo",
+    hourText: "Hora",
+    minuteText: "Minuto",
+    secondText: "Segundo",
+    ampm: false,
+    month: "Mês",
+    week: "Semana",
+    day: "Dia",
+    allDayText: "Todo Dia",
+    today: "Hoje",
+    clear: "Limpar",
+  });
+
   useEffect(() => {
     setCurrentPage("Relatórios");
   }, [setCurrentPage]);
 
   return (
     <div className="p-p-3 ">
-      <div className="p-p-3 p-d-flex p-ai-center p-jc-center default-container">
-        <div className="p-d-flex p-grid nav-reports p-mb-3">
+      <div className="p-p-3 p-ai-center p-jc-center default-container">
+        <div className="p-d-flex p-grid p-mb-3 calendar-select">
           <div className="p-field p-col-12 p-md-2 p-as-center p-m-0 p-justify-center p-d-flex">
             <label className="p-as-center p-text-center p-mb-0">
               Período Inicial&emsp;
@@ -43,8 +108,9 @@ function Reports() {
           </div>
           <div className="p-field p-col-12 p-md-3 p-as-center p-m-0 p-justify-center p-d-flex">
             <Calendar
-              className="visits-button"
+              className="calendar-button"
               showIcon
+              locale="pt"
               dateFormat="dd/mm/yy"
               value={startDate}
               onChange={(e) => setStartDate(new Date(e.target.value))}
@@ -57,7 +123,8 @@ function Reports() {
           </div>
           <div className="p-field p-col-12 p-md-3 p-as-center p-m-0 p-justify-center p-d-flex">
             <Calendar
-              className="visits-button"
+              className="calendar-button"
+              locale="pt"
               showIcon
               dateFormat="dd/mm/yy"
               value={endDate}
@@ -65,12 +132,41 @@ function Reports() {
             />
           </div>
           <div className="p-field p-col-12 p-md-2 p-as-center p-m-0 p-justify-center p-d-flex">
-            <Button className="button-primary" onClick={() => searchReport()}>
+            <Button className="login-button" onClick={() => searchReport()}>
               Consultar
             </Button>
           </div>
         </div>
-        <div className="graphics-reports"></div>
+        <div className="p-grid">
+          <div className="graphics-reports p-col-12 p-md-12">
+            <p className="p-p-0 p-m-0">
+              <b>Quantidade de Votos por Avaliação</b>
+              <br />
+              135
+              <small>
+                <br />
+                {`No Período: ${moment(searchStartDate).format(
+                  "DD/MM/YYYY",
+                )} até ${moment(searchEndDate).format("DD/MM/YYYY")}`}
+              </small>
+            </p>
+            <BarChart />
+          </div>
+          <div className="graphics-reports p-col-12 p-md-12">
+            <p className="p-p-0 p-m-0">
+              <b>Faturamento</b>
+              <br />
+              30
+              <small>
+                <br />
+                {`No Período: ${moment(searchStartDate).format(
+                  "DD/MM/YYYY",
+                )} até ${moment(searchEndDate).format("DD/MM/YYYY")}`}
+              </small>
+            </p>
+            <BarChart />
+          </div>
+        </div>
       </div>
     </div>
   );
